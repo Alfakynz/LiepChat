c4fb807783407daaa542df77240e50f5d61e5b40
Alfakynz <141228129+Alfakynz@users.noreply.github.com>
2024-10-24 23:10:50 +0200

    Ajout du markdown + ajout du focus automatique
    - Ajout du markdown
    - Ajout des focus automatique
    - Ajout du texte dans la page Welcome
    - Corrections de bugs

--- config/script.js
+++ config/script.js
@@ -73,8 +73,13 @@ function fixHTML(html) {
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;')
-    .replace(/\n/g, '<br />');
+    .replace(/\n/g, '<br />')
+    .replace(/\*(.*?)\*/g, '<b>$1</b>')
+    .replace(/_(.*?)_/g, '<i>$1</i>')
+    .replace(/~(.*?)~/g, '<s>$1</s>')
+    .replace(/__(.*?)__/g, '<u>$1</u>');
 }
+
 
 function getHTMLMessage(message, formattedDate, username, isCertified, color, image) {
   var img;
