e724903670984bdc7d8ce93ffa1c104c3b02ac96
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-05-15 13:56:20 +0200

    Update to Vuejs

--- config/language.js
+++
@@ -1,8 +0,0 @@
-function getPrincipalLanguage(acceptLanguage) {
-  if (acceptLanguage && acceptLanguage.length >= 2) {
-    return acceptLanguage.slice(0, 2);
-  }
-  return 'en';
-}
-
-module.exports = { getPrincipalLanguage };
