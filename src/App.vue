8ecf021cb457c66eeddc50187229303954d4eb3b
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-05-25 20:17:36 +0200

    Code split + ThemeButton added
    - ThemeButton added (@Firelack)
    - Sass files split
    - NavItem updated
    - Better UI/UX (@Firelack)
    
    Co-Authored-By: Sarah D <91343692+firelack@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 import { useRoute } from 'vue-router'
 import NavView from './components/NavView.vue'
+import ThemeButton from './components/ThemeButton.vue'
 
 const route = useRoute()
 </script>
 
 <template>
   <header>
+    <RouterLink to="/">
+      <img src="/src/assets/images/logos/logo.png" alt="Website Logo" class="logo" />
+    </RouterLink>
     <h1>{{ route.meta.title || 'Page' }}</h1>
+    <ThemeButton />
   </header>
 
   <NavView />
---
+++ src/components/NavItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps<{
+  to: string;
+  label: string;
+}>();
+</script>
+
+<template>
+  <RouterLink :to="to" custom v-slot="{ navigate, href }">
+    <a :href="href" @click="navigate">
+      <slot name="icon"></slot>
+      <div>{{ label }}</div>
+    </a>
+  </RouterLink>
+</template>
