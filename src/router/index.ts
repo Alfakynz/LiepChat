43cba05df2b9d83ce39aaefec98c3de4371b58fe
Firelack <firelack2000@gmail.com>
2025-05-26 16:58:18 +0200

    Add 404 error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import SigninView from '../views/SigninView.vue'
 import SignupView from '../views/SignupView.vue'
+import NotFoundView from '../views/404View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -40,7 +41,12 @@ const router = createRouter({
         title: 'Sign up',
       },
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
+      meta: { title: '404 - Not Found' },
+    },
   ],
 })
-
 export default router
---
+++ src/views/404View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goBack() {
+  router.back()
+}
+</script>
+
+<template>
+  <section>
+    <p>Page not found</p>
+    <p><a href="#" @click.prevent="goBack">Back</a></p>
+  </section>
+</template>
