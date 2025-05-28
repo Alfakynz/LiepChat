40adcc6ca8034a29e4ec22784ef14dfc60ae99c5
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-05-28 19:29:23 +0200

    Date bug fixed

--- config/script.js
+++ config/script.js
@@ -38,7 +38,7 @@ function getFormattedDate(date, atText) {
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
   const year = date.getFullYear();
 
-  const hours = ("0" + (date.getHours())).slice(-2);
+  const hours = ("0" + ((date.getUTCHours() + 2) % 24)).slice(-2);
   const minutes = ("0" + date.getMinutes()).slice(-2);
   const seconds = ("0" + date.getSeconds()).slice(-2);
 
